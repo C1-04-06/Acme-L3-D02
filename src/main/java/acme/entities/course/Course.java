e461875b77d2c2ed411c2f27a17deafa4b88c7dc
ursblue <ugsugs333@gmail.com>
2023-03-02 16:40:19 +0100

    Patrón modificado

--- src/main/java/acme/entities/course/Course.java
+++ src/main/java/acme/entities/course/Course.java
@@ -28,7 +28,7 @@ public class Course extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}\\s\\d{3}")
 	@Column(unique = true)
 	protected String			code;
 
