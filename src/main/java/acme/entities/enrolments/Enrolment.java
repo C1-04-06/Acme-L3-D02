d9be7ca80f4a4c42c95d7f3c05fd302db9e3c636
marmarsol4 <marmarsol4@alum.us.es>
2023-03-10 21:56:57 +0100

    Csvs estudiante 2 y correcciones en entidades

--- src/main/java/acme/entities/enrolments/Enrolment.java
+++ src/main/java/acme/entities/enrolments/Enrolment.java
@@ -28,7 +28,7 @@ public class Enrolment extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 
-	@Pattern(regexp = "[A-Z]{1,3}\\d{3}")
+	@Pattern(regexp = "^[A-Z]{1,3}\\d{3}$")
 	@NotBlank
 	@Column(unique = true)
 	protected String			code;
@@ -45,7 +45,7 @@ public class Enrolment extends AbstractEntity {
 
 	// Derived attributes -----------------------------------------------------
 
-	// Double getWorkbookTime() -> we cannot implement it yet
+	// Integer getWorkbookTime() -> we cannot implement it yet. Total time in hours
 
 	// Relationships ----------------------------------------------------------
 
