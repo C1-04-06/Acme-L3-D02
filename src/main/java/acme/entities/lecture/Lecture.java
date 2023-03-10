3afc9329e7abc26b497a210e11ed6d06f022bcde
ursgarsan <ursgarsan@alum.us.es>
2023-03-10 12:34:51 +0100

    Añadido formato nuevo de time

--- src/main/java/acme/entities/lecture/Lecture.java
+++ src/main/java/acme/entities/lecture/Lecture.java
@@ -4,8 +4,11 @@ package acme.entities.lecture;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
@@ -34,6 +37,12 @@ public class Lecture extends AbstractEntity {
 	@NotBlank
 	@Length(max = 100)
 	protected String			abstr;
+
+	//tres dígitos enteros, dos dígitos fraccionales, y mínimo valor 0.01.
+	@Positive
+	@Digits(integer = 3, fraction = 2)
+	@DecimalMin(value = "0.01", inclusive = true)
+	protected double			time;
 
 	@NotNull
 	protected Nature			lectureNature;
