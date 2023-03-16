6c47f304ccc18a97d6aac317476d27feed1afd68
diegarlin <diegarlin@alum.us.es>
2023-03-16 21:08:53 +0100

    AdministratorDashboard hecho, cambio en auditor, modelo actualizado

--- src/main/java/acme/forms/AdministratorDashboard.java
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -1,19 +1,31 @@
 
 package acme.forms;
 
+import java.util.Map;
+
+import acme.framework.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class AdministratorDashboard extends Statistics {
+public class AdministratorDashboard extends AbstractForm {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long serialVersionUID = 1L;
+	protected static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
 
+	Map<String, Integer>		numberOfPrincipalsByRole;
+
+	Double						ratioOfPeepsWithEmailAndLink;
+
+	Map<Boolean, Double>		ratioOfBulletinByCritical;
+
+	Map<String, Statistics>		budgetByCurrencyStats;
+
+	Statistics					notesStats;
 	// Derived attributes -----------------------------------------------------
 
 	// Relationships ----------------------------------------------------------
